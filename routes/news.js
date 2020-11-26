6698f6b04ff383bcbc46812f331ba72654c57785
Dominika <dominikadmt@gmail.com>
2020-11-26 13:38:52 +0100

    news filter

--- routes/news.js
+++ routes/news.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const News = require('../models/news')
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('news', { title: 'News' });
+  // Pobranie danych z wyszukiwarki
+  console.log(req.query);
+  const search = req.query.search || '';
+
+
+  const findNews = News
+  // trim - pomijanie spacji
+  .find({title: new RegExp(search.trim(), 'i')})
+  .sort({created: -1});
+  //  -1 malejąco, 0 domyslnie, 1 rosnąco
+  
+  findNews.exec((err, data) => {
+    // console.log(data);
+    res.render('news', { title: 'News', data, search });
+  })
+
 });
 
 module.exports = router;
